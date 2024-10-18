62dd02140baf2c4da68b37af3850f1e490cd98af
Umer Saleem <162819866+UmerSaleemUltra@users.noreply.github.com>
2024-10-18 05:26:36 -0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
 //connection string
-mongoose.connect('mongodb+srv://umer:171175@cluster0.zbqcm.mongodb.net/olx?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect('');
 
 export default mongoose
